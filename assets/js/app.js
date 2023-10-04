81826427f2f67f7a20d83dd0545d62c1a392a2ec
AxelPacheco <ing.axpacheco@gmail.com>
2023-10-04 15:55:42 -0600

    Se arreglo y funciona la API

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,23 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
+const $n = document.querySelector('.name');//se agrefo punto
+const $b = document.querySelector('.blog'); //se cambio # por un punto
 const $l = document.querySelector('.location');
 
-function displayUser(username) {
+async function displayUser(username) { //se agrego la palabra async para que se pueda imprimir en la consola
   $n.textContent = 'cargando...';
   const response = await fetch(`${usersEndpoint}/${username}`);
+  const data = await response.json(); //se agrego la constante data para que se pueda imprimir en la consola
   console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  $n.textContent = `${data.name}`;//se corrigieron las comillas en los tres parrafos
+  $b.textContent = `${data.blog}`;
+  $l.textContent = `${data.location}`;
 }
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  n.textContent = `Algo salió mal: ${err}`;
 }
 
 displayUser('stolinski').catch(handleError);
